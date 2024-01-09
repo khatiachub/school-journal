5d33c0af87e4d24013cd89b38dc3f35489e75ac8
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-01-09 22:59:58 +0400

    change server url

--- client/src/components/requestmethods.tsx
+++ client/src/components/requestmethods.tsx
@@ -10,7 +10,7 @@ const TOKEN = token?.replace(/^"(.*)"$/, '$1');
 console.log(TOKEN);
 
 
-const BASE_URL = "http://localhost:5002";
+const BASE_URL = "https://school-journal-gray.vercel.app";
 export const publicRequest = axios.create({
     baseURL: BASE_URL
   });
